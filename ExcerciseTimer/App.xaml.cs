bb9263bfe8f8a10a2a850595164457e5b45a593c
Viktor Riabtsev <viktor.riabtsev@live.ca>
2015-11-21 11:24:21 -0500

    First commit. Redirected main app code from startupuri to a method in App class.

---
+++ ExcerciseTimer/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExcerciseTimer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Main application method. Called at some point after App was constructed in the generated static Main() method.
+        /// Linked to by App.xaml.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ApplicationEntryPoint(object sender, StartupEventArgs e)
+        {
+            Console.WriteLine("Hello world.");
+        }
+    }
+}
